e19b8136c3dab8fbda2794c690081e803c294321
MINAS\User <ics20072@uom.edu.gr>
2021-12-12 17:36:10 +0200

    initial commit

---
+++ Default.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body style="background-color: #C7E1EB">
+    <form id="form1" runat="server">
+        <asp:Label ID="Label1" runat="server" Text="Calculator" style="font-size: xx-large; font-weight: 700"></asp:Label>
+        <br />
+        <br />
+        <div>
+            <asp:Label ID="Label2" runat="server" Text="Number A:" style="font-weight: 700; font-size: large"></asp:Label>
+        &nbsp;
+            <asp:TextBox ID="InputNumberA" runat="server" BorderColor="Black" ToolTip="Give the first Number"></asp:TextBox>
+    
+            <br />
+            <br />
+            <asp:ImageButton ID="addButton" runat="server" ImageUrl="~/images/add_35px.png" OnClick="addButton_Click" />
+    &nbsp;&nbsp;&nbsp;
+            <asp:ImageButton ID="multiplicationButton" runat="server" ImageUrl="~/images/multiply_35px.png" OnClick="multiplicationButton_Click" />
+    &nbsp;&nbsp;&nbsp;
+            <asp:ImageButton ID="divisionButton" runat="server" ImageUrl="~/images/divide_35px.png" OnClick="divisionButton_Click" />
+    &nbsp;&nbsp;&nbsp;
+            <asp:ImageButton ID="subtractionButton" runat="server" ImageUrl="~/images/minus_35px.png" OnClick="subtractionButton_Click" />
+    &nbsp;&nbsp;&nbsp;
+            <asp:ImageButton ID="resetButton" runat="server" ImageUrl="~/images/reset_35px.png" ToolTip="Reset All" OnClick="resetButton_Click" />
+            <br />
+            <br />
+            <asp:Label ID="Label3" runat="server" Text="Number B:" style="font-weight: 700; font-size: large"></asp:Label>
+        &nbsp;
+            <asp:TextBox ID="InputNumberB" runat="server" BorderColor="Black" ToolTip="Give the second Number"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Label ID="Label4" runat="server" Text="Result: " style="font-weight: 700; font-size: x-large"></asp:Label>
+            <asp:Label ID="theResultLabel" runat="server" Text="" style="font-weight: 700; font-size: large; color: #CC0000; font-style: italic;"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    com.dneonline.www.Calculator myWS = new com.dneonline.www.Calculator();
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected Boolean notEmptyFields(string fieldNumberA, string fieldNumberB)
+    {
+        if (string.Equals(fieldNumberA, "") && string.Equals(fieldNumberB, ""))
+        {
+            InputNumberA.BorderColor = System.Drawing.Color.Red;
+            InputNumberB.BorderColor = System.Drawing.Color.Red;
+            theResultLabel.Text = "";
+            return false;
+        }else if (string.Equals(fieldNumberA, ""))
+        {
+            InputNumberA.BorderColor = System.Drawing.Color.Red;
+            InputNumberB.BorderColor = System.Drawing.Color.Black;
+            theResultLabel.Text = "";
+            return false;
+        }else if (string.Equals(fieldNumberB, ""))
+        {
+            InputNumberB.BorderColor = System.Drawing.Color.Red;
+            InputNumberA.BorderColor = System.Drawing.Color.Black;
+            theResultLabel.Text = "";
+            return false;
+        }else
+        {
+            InputNumberA.BorderColor = System.Drawing.Color.Black;
+            InputNumberB.BorderColor = System.Drawing.Color.Black;
+            return true;
+        }
+    }
+
+    protected void addButton_Click(object sender, ImageClickEventArgs e)
+    {
+        if (notEmptyFields(InputNumberA.Text, InputNumberB.Text))
+        {
+            int numberA, numberB;
+
+            numberA = Convert.ToInt32(InputNumberA.Text);
+            numberB = Convert.ToInt32(InputNumberB.Text);
+
+            theResultLabel.Text = Convert.ToString(myWS.Add(numberA, numberB));
+        }
+    }
+
+    protected void multiplicationButton_Click(object sender, ImageClickEventArgs e)
+    {
+        if (notEmptyFields(InputNumberA.Text, InputNumberB.Text))
+        {
+            int numberA, numberB;
+
+            numberA = Convert.ToInt32(InputNumberA.Text);
+            numberB = Convert.ToInt32(InputNumberB.Text);
+
+            theResultLabel.Text = Convert.ToString(myWS.Multiply(numberA, numberB));
+        }
+    }
+
+    protected void divisionButton_Click(object sender, ImageClickEventArgs e)
+    {
+        if (notEmptyFields(InputNumberA.Text, InputNumberB.Text))
+        {
+            double numberA, numberB;
+
+            numberA = Convert.ToDouble(InputNumberA.Text);
+            numberB = Convert.ToDouble(InputNumberB.Text);
+
+            if (numberB != 0)
+            {
+                double result = numberA / numberB;
+                theResultLabel.Text = Convert.ToString(result);
+            }
+            else
+            {
+                theResultLabel.Text = "The denominator with 0 in the denominator cannot be set";
+            }
+        }
+    }
+
+    protected void subtractionButton_Click(object sender, ImageClickEventArgs e)
+    {
+        if (notEmptyFields(InputNumberA.Text, InputNumberB.Text))
+        {
+            int numberA, numberB;
+
+            numberA = Convert.ToInt32(InputNumberA.Text);
+            numberB = Convert.ToInt32(InputNumberB.Text);
+
+            theResultLabel.Text = Convert.ToString(myWS.Subtract(numberA, numberB));
+        }
+    }
+
+    protected void resetButton_Click(object sender, ImageClickEventArgs e)
+    {
+        InputNumberA.Text = "";
+        InputNumberB.Text = "";
+        theResultLabel.Text = "";
+        InputNumberB.BorderColor = System.Drawing.Color.Black;
+        InputNumberA.BorderColor = System.Drawing.Color.Black;
+    }
+}
